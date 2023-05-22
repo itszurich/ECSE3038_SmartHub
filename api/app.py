8af959b289fce563a64f1604fc75066ae3bc8988
Zurich Hentley <zurichentley@gmail.com>
2023-05-21 22:58:30 -0500

    added status codes

--- api/app.py
+++ api/app.py
@@ -1,7 +1,8 @@
-from json import dumps
+import json 
 from typing import Optional
 from fastapi import FastAPI, Request, HTTPException, status
-from fastapi.responses import Response
+from fastapi.responses import Response, JSONResponse
+from fastapi.encoders import jsonable_encoder
 from bson import ObjectId
 from httpx import request
 import motor.motor_asyncio
@@ -26,6 +27,9 @@ pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
 
 origins=[
     "https://simple-smart-hub-client.netlify.app"
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+ 
 ]
 
 app.add_middleware(
@@ -36,34 +40,38 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+res =  request.get("https://api.sunrise-sunset.org/json?lat=18.1096&lng=-77.2975&formatted=0")
+resJson = res.json()
+sunSet_Time = resJson["sunset"]
+sunRise_Time = resJson["sunrise"]
+sunSetTime = datetime.datetime.strptime(sunSet_Time["sunset"].split("T")[1],"%H:%M:%S.%f")
+sunRiseTime = datetime.datetime.strptime(sunRise_Time["sunrise"].split("T")[1],"%H:%M:%S.%f")
+currentTime= datetime.datetime.now().strftime("%H:%M:%S.%f")
+current_time = datetime.datetime.strptime(currentTime,"%H:%M:%S.%f")
 
-@app.get("/api/graph",{"amount"})
-async def read_item(skip: int = 0, limit:int= ["amount"]):
-    graphmax = request.json["graphMax"]
-    record= MongoDsn.db.parameters.find({"temps":graphmax})
-    return dumps(list(record))
 
-@app.get("/api/states") #gets states of both the temp and sunset
+@app.get("/graph",status_code=200)
+async def graph(request: Request, size: int):
+    graph_obj = await db["Embedded"].find().sort('_id', -1).to_list(size)
+    graphList = []
+
+    for obj in graph_obj:
+        temps = obj["temperature"]
+        pres = obj["presence"]
+        datetime = obj["created"]
+        graphStuff = {"temperature": temps, "presence": pres, "datetime": datetime}
+        await db["Graph"].insert_one(graphStuff)
+        graphList.append(graphStuff)
+    graphList = [{**data, "_id": str(data["_id"])} for data in graphList]
+    return JSONResponse(content=graphList)
+
+@app.get("/api/states",status_code=200) #gets states of both the temp and sunset
 async def get_states():
 
-    
-
-    temperature = await db["temps"].find().sort("date",-1).to_list(1)
-    time = await db["time"].find().sort("date").to_list(1)
-    pressence = await db["detected"].find().sort("date",-1).to_list(1)
+    temperature = await db["temperature"].find().sort("date",-1).to_list(1)
+    pressence = await db["presence"].find().sort("datetime",-1).to_list(1)
     lamp=False
     fan = False
-
-    res =  request.get("https://api.sunrise-sunset.org/json")
-    resJson = res.json()
-    sunSet_Time = resJson["sunset"]
-    sunRise_Time = resJson["sunrise"]
-    sunSetTime = datetime.datetime.strptisme(sunSet_Time["sunset"].split("T")[1],"%H:%M:%S.%f")
-    sunRiseTime = datetime.datetime.strptime(sunRise_Time["sunrise"].split("T")[1],"%H:%M:%S.%f")
-    currentTime= datetime.datetime.now().strftime("%H:%M:%S.%f")
-    current_time = datetime.datetime.strptime(currentTime,"%H:%M:%S.%f")
-    
-    
     
     if len(temperature)==0:
         return{
@@ -77,8 +85,8 @@ async def get_states():
         }
 
     if pressence == True:
-        settemp=request.json["temps"]
-        if temperature[0]["temps"] >= settemp:
+        settemp=request.json["temperature"]
+        if temperature[0]["temperature"] >= settemp:
             fan = True
         if (current_time >= sunSetTime) & (current_time<sunRiseTime):
             lamp = True
@@ -93,14 +101,25 @@ async def get_states():
     }
 
 
-@app.put("/api/temperature",status_code=204)
-async def set_temp(request:Request):
+@app.put("/api/put",status_code=204)
+async def set_temp_time(request:Request):
+     info_obj = await request.json()
+     if info_obj["user_light"] == "sunset" :
+        info_obj["user_light"]= sunSetTime
+        time_off= sunSetTime + parse_time(info_obj["light_duration"])
+        info_obj["light_time_off"] = time_off.strftime("%H:%M:%S")
+     else: 
+        info_obj["user_light"]!="sunset"
+        user_light = datetime.strptime(info_obj["user_light"], "%H:%M:%S").time()     #making TIME object from user_light parameter
+        today = date.today()                                                                #fetching current date
+        user_date_time = datetime.combine(today, user_light)                             #attaching current date to user light time
+        time_off= user_date_time + parse_time(info_obj["light_duration"])
+        info_obj["light_time_off"] = datetime.strftime(time_off_dto, "%H:%M:%S")
     try:
         temp_obj = await request.json()
-        temp_obj["date"]=datetime.datetime.now()
-        new_temp=await db["temps"].insert_one(temp_obj)
-        created_temp = await db["temps"].find_one({"_id": new_temp.inserted_id})
-
+        temp_obj["datetime"]=datetime.datetime.now()
+        new_temp=await db["temperature"].insert_one(temp_obj)
+        # created_temp = await db["temperature"].find_one({"_id": new_temp.inserted_id})
     except:
         raise HTTPException(status_code=400,detail="you goofed")
     
